83f91260e1227a9251e3e5fbbccc7650f26454db
antoniomf97 <antonio.mf97@gmail.com>
2025-06-19 22:16:23 +0100

    Updated gateway with simple version

--- services/gateway/server.py
+++ services/gateway/server.py
@@ -1,39 +1,15 @@
 import os
 
-
 import uvicorn
 from decouple import config
-from fastapi import FastAPI, File, UploadFile, HTTPException, Depends
-from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
-from jose import JWTError, jwt
+from fastapi import FastAPI, File, UploadFile, HTTPException
 
 
-app = FastAPI(title="gateway")
-
-
-# Security configuration
-SECRET_KEY = "your-very-secret-key"
-ALGORITHM = "HS256"
-security = HTTPBearer()
-
-
-def verify_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
-    try:
-        payload = jwt.decode(credentials.credentials, SECRET_KEY, algorithms=[ALGORITHM])
-        return payload  # could include roles, user_id, etc.
-    except JWTError:
-        raise HTTPException(status_code=403, detail="Invalid token")
-
-
-UPLOAD_DIR = "./uploads"
-os.makedirs(UPLOAD_DIR, exist_ok=True)
+app = FastAPI(title="api")
 
 
 @app.post("/")
-async def plot_request(
-        file: UploadFile = File(...),
-        token_data: dict = Depends(verify_token)
-    ):
+async def plot_request(file: UploadFile = File(...)):
     if file.content_type not in ["text/plain", "text/csv"]:
         raise HTTPException(status_code=400, detail="File type not allowed")
 
