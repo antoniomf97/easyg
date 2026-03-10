447c03953b13ec3b38d11cd78e6c2b6e164e2ef7
antoniomf97 <antonio.mf97@gmail.com>
2026-03-10 15:44:52 +0000

    added last unittests

--- backend/tests/integration/api/test_routes.py
+++ backend/tests/integration/api/test_routes.py
@@ -4,23 +4,18 @@ import json
 
 client = TestClient(app)
 
+
 def test_root_endpoint(client):
     response = client.get("/")
 
     assert response.status_code == 200
     assert response.json() == "Welcome to easyg"
 
-def test_plotter_success(client):
-    files = {
-        "file": ("test.csv", b"x,y,z\n1,2,3\n3,4,5", "text/csv")
-    }
 
-    data = {
-        "configs": json.dumps({
-            "x": "col1",
-            "y": "col2"
-        })
-    }
+def test_plotter_success(client):
+    files = {"file": ("test.csv", b"x,y,z\n1,2,3\n3,4,5", "text/csv")}
+
+    data = {"configs": json.dumps({"x": "col1", "y": "col2"})}
 
     response = client.post("/plotter", files=files, data=data)
 
